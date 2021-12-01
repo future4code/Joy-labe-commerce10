299ba8d17b1777692eee33d9b1a9f877af966b8e
Jonathan-Andrade <sjonatask@gmail.com>
2021-11-30 21:17:18 -0300

    esqueleto e criação dos componentes feito por live share com a equipe inteira

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from "./componentes/Home"
+import Carrinho from "./componentes/Carrinho/Carrinho"
+import Filtro from "./componentes/Filtro"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    card: [
+      {
+        // id: 
+        // nome:
+        // valor:
+        // imagen:
+      }
+    ]
+  }
+  render(){
+    
+
+    return (
+      <div className="App">
+        <Filtro/>
+        <Home/>
+        <Carrinho/>
+      </div>
+    );
+  }
 }
 
 export default App;
